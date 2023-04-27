f60568a5b59691d67c0ad8fa9e57aea112788127
fturello <92311869+fturello@users.noreply.github.com>
2023-04-27 11:51:09 +0200

    removed unused import

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 
 import router from "./routers";
